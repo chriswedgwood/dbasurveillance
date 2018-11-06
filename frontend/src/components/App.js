bbd48aa404a22b59971c6fca11aed18a2d874f75
chris wedgwood <chriswedgwood@tfl.gov.uk>
2018-11-06 11:53:54 +0000

    Tuesday fix

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,13 +1,23 @@
-import React from "react";
+import React, {Component} from 'react';
 import ReactDOM from "react-dom";
 import DataProvider from "./DataProvider";
 import Graph from "./Graph";
 
 
-const App = () => (
-  <DataProvider endpoint="api/cpu/"
-                render={data => <Graph data={data} />} />
-);
+class App extends Component {
+    render() {
+        return (
+           <span>
+        <DataProvider
+          endpoint="api/cpu/"
+          render={data => <Graph data={data} />}
+        />
+      </span>
+        );
+    }
+}
+
+App.propTypes = {};
 
 const wrapper = document.getElementById("app");
 
